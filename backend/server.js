da827b8ad6285f8b6f6a663e6a8e00e67a870fd5
divyesh_06 <divyeshlakhotia@gmail.com>
2024-10-26 18:06:24 +0530

    Handling routes for all paths

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ server.use(cors())
 server.use(express.json());
 
 //Serve react app
-
-server.get("/", function(req, res) {
-    const filePath = path.resolve(__dirname, '../frontend/build/index.html');
-    res.sendFile(filePath);
-})
-
 server.post("/contact", function(req, res) {
     console.log(req.body)
     process_contact_info(req.body)
     res.status(200).send("Success")
 })
+
+server.get("*", function(req, res) {
+    const filePath = path.resolve(__dirname, '../frontend/build/index.html');
+    res.sendFile(filePath);
+})
+
 
 server.listen(10000)
